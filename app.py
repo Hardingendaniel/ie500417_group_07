d9f4484843160773a3035c3c64ad7fa57ff8b365
emmanes <Emma.nesvik@hotmail.com>
2024-11-27 10:20:49 +0100

    Uncommented prediction
    Forgot to uncomment this before I pushed, I had it  commented since the YE / A thing and such takes 5 years to load

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # app.py
 from dash import Dash, html, dcc
 import pages.analyse as analyse
-#import pages.prediction as prediction
+import pages.prediction as prediction
 import pages.visualization1 as visualization1
 import pages.visualizationOfEurope as visualizationOfEurope
 
@@ -15,7 +15,7 @@ analyse.init_callbacksalso(app)
 app.layout = html.Div([
     dcc.Tabs([
         dcc.Tab(className='tabs-title',label='Analyse', children=analyse.layout),
-        #dcc.Tab(className='tabs-title',label='Prediction', children=prediction.layout),
+        dcc.Tab(className='tabs-title',label='Prediction', children=prediction.layout),
         dcc.Tab(className='tabs-title',label='Visualization', children=visualization1.layout),
         dcc.Tab(className='tabs-title',label='Visualization of Europe and Arctic Winter', children=visualizationOfEurope.layout)
     ])
